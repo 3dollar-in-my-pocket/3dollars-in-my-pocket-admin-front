6e8f7a04660cdb16cc387cd57b7acd9e125eb56e
seungh0 <will.seungho@gmail.com>
2023-11-06 02:08:20 +0900

    이미지 종류를 선택할 수 있도록 변경

--- src/apis/UploadApi.js
+++ src/apis/UploadApi.js
@@ -4,11 +4,11 @@ import { AUTH_KEY, AUTH_TOKEN } from 'constants';
 import { LocalStorageService, HttpService } from 'services';
 
 export default {
-  upload: async (file) => {
+  upload: async (selectedImageType, file) => {
     const formData = new FormData();
     formData.append('file', file);
     return axios.post(
-      `${AUTH_KEY.apiUrl}/admin/v1/file/ADVERTISEMENT_IMAGE`,
+      `${AUTH_KEY.apiUrl}/admin/v1/file/${selectedImageType}`,
       formData,
       HttpService.withBearer(LocalStorageService.get(AUTH_TOKEN))
     );
